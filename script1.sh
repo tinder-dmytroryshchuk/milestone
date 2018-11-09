3b11ff448c0890cfa86c81dd48b7fed84d5d5e17
Dmytro Ryshchuk <dmytro.ryshchuk@gotinder.com>
2018-11-09 11:44:26 +0200

    script

---
+++ script1.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+version=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "mile/mile/Info.plist")
+
+MAJOR="$( cut -d '.' -f 1 <<< "$version" )"
+MINOR="$( cut -d '.' -f 2 <<< "$version" )"
+PATCH="$( cut -d '.' -f 3 <<< "$version" )"
+
+CURRENT_VERSION=release/${MAJOR}.${MINOR}.0
+echo $CURRENT_VERSION
+
+echo "Verify release branch is deleted"
+curl https://api.github.com/repos/tinder-dmytroryshchuk/milestone/branches/$CURRENT_VERSION \
+-H "Authorization: token $1" | jq -r ".name" > release_exist
+read verify <<< $(awk 'NR==1' release_exist)
+rm -f release_exist
+
+if [ $verify = $CURRENT_VERSION ]; then
+	echo "$CURRENT_VERSION branche still exists"; exit
+fi
+
+echo "Bump Version"
+NEW_MINOR=$(($MINOR+1))
+
+NEW_VERSION=${MAJOR}.${NEW_MINOR}.0
+
+build_number=$(printf "%d%02d%02d00" "$MAJOR" "$NEW_MINOR" 0)
+
+/usr/libexec/PlistBuddy -c "Set CFBundleVersion $build_number" "mile/mile/Info.plist"
+/usr/libexec/PlistBuddy -c "Set CFBundleShortVersionString $NEW_VERSION" "mile/mile/Info.plist"
+
+echo "Committing the change"
+git add mile/mile/Info.plist
+echo "NOW======1"
+git commit -m "Preparing for next release $NEW_VERSION"
+git push
+echo "NOW======2"
+
+NEW_BRANCH=release/${MAJOR}.$(($MINOR+1)).0
+echo "Creating branch $NEW_BRANCH"
+git checkout -b $NEW_BRANCH
+echo NEW_BRANCH=${NEW_BRANCH} >new_branch
+echo "NOW======3"
+
+echo "Pushing"
+git push --set-upstream origin $NEW_BRANCH
+echo "NOW======4"
